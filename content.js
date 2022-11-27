a0e838cd4a310e00ede27a82da11976e497a8365
SmValeriya <lerok1000@gmail.com>
2022-11-27 19:56:18 +0200

    Add html content

--- content.js
+++ content.js
@@ -1,16 +1,36 @@
 export const data = {
-  "team": [
-    [
-      "дэниэл редклифф", "Шеф-повар"
-    ],
-    [
-      "ричард джеймс", "Оператор"
-    ],
-    [
-      "Джейк Финн", "Менеджер"
-    ],
-    [
-      "Николай Иванов", "Шоколатье"
-    ]
+  "questions": [
+    {
+      id: "q1",
+      text: "How do you give a ninja directions?",
+      answers: {
+        A: "Show them a map",
+        B: "Don't worry, a ninja will find you",
+      },
+    },
+    {
+      id: "q2",
+      text: "If a ninja has 3 apples, then gives one to Mario & one to Yoshi, how many apples does the ninja have left?",
+      answers: {
+        A: "1 apple",
+        B: "3 apples, and two corpses",
+      },
+    },
+    {
+      id: "q3",
+      text: "How do you know when you've met a ninja?",
+      answers: {
+        A: "You'll recognize the outfit",
+        B: "The grim reaper will tell you",
+      },
+    },
+    {
+      id: "q4",
+      text: "What's a ninja's favourite array method?",
+      answers: {
+        A: "forEach()",
+        B: "slice()",
+      },
+    },
   ]
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,1 @@
-import { test } from './common/test.js';
-
 console.log('index page here');
-
-test();
